19b18d703fa90c8cfbf973586ceab7a17aeaab4c
Rachneet Kaur <rk4@illinois.edu>
2021-07-13 01:00:17 -0500

    Update select.py

--- apps/select.py
+++ apps/select.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def app():
     st.title("Description")
-    st.header("This is the website for the paper. #TODO paper link")
+    st.header("This is the website for the paper [Predicting Alzheimer’s disease progression trajectory and clinical subtypes using machine learning](https://www.biorxiv.org/content/10.1101/792432v2)**")
     # st.write("## Summary")
     # st.write("### Background")
     # st.write("The disease entity known as amyotrophic lateral sclerosis (ALS) is now known to represent a collection of overlapping syndromes. A better understanding of this heterogeneity and the ability to distinguish ALS subtypes would improve the clinical care of patients and enhance our understanding of the disease. Subtype profiles could be incorporated into the clinical trial design to improve our ability to detect a therapeutic effect. A variety of classification systems have been proposed over the years based on empirical observations, but it is unclear to what extent they genuinely reflect ALS population substructure.")
